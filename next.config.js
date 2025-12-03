8aa87d83f8b114fdafe2ad897dd8298e529d4a7e
Haendell Lopes <haendell.lopes@gmail.com>
2025-12-03 15:22:26 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   transpilePackages: ['three'],
   typescript: {
@@ -33,4 +32,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
